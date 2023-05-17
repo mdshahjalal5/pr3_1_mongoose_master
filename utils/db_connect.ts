e3cc97b2777d08950f88eebdb11b5c5ce489595a
mdshahjalal2 <muhommodshahjalal@gmail.com>
2023-05-17 21:28:19 +0600

    part 7 has done

--- utils/db_connect.ts
+++ utils/db_connect.ts
@@ -4,10 +4,22 @@ import mongoose, { ConnectOptions } from "mongoose";
 // Define the MongoDB connection URL
 const dbUrl =
   "mongodb+srv://web2:8HdqwXJGb4ZKIcJ1@chkdbcluster.qsaqded.mongodb.net/?retryWrites=true&w=majority";
+  
+  
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000,
+  autoIndex: true,
+  maxPoolSize: 10,
+  socketTimeoutMS: 45000,
+  family: 4,
+  dbName: "DataBase-change-2",
+};
 
 const db_connect = async() =>{
 try {
-    await mongoose.connect(dbUrl);
+    await mongoose.connect(dbUrl, options);
     console.log('Db connection successful');
     
 } catch (err) {
